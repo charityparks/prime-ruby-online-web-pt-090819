825397ef797cf7fd4b711536dc045d0c3bb30bd9
Charity Parks <tuppercharity@gmail.com>
2019-09-20 00:29:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
 def prime?(num)
   if num < 2
     return false
-  else
-
-  (2..num - 1).any? do | n |
-    num % n != 0
   end
-end
+
+    (2..num - 1).any? do | n |
+      num % n != 0
+    end
+  
 end
 
 # 3 % 2 = 1
