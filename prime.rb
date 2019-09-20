770ada7f6ae234d1b9b47aaa86e810d291428322
Charity Parks <tuppercharity@gmail.com>
2019-09-20 00:21:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,12 @@
-def prime?(num)
+def prime?(num = 7)
   if num < 2
     false
   end
-  
-  (2..num).each
-    # num % n == 0
+  [2,3,4,5,6]
+  (2..num).any? do | number |
+    
+    
+  end
 end
 
 # 3 % 2 = 1
